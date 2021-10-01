40da84d2cb491bf363b1d28a40910adbe5082e94
Oluwafemi Ayinla <oluwafemi.ayinla@gmail.com>
2021-10-01 13:58:12 +0100

    updated-css-js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,3 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
- */
-
-/**
- * Define Global Variables
- * 
- */
 let navContainer = document.querySelector('#navbar__list');
 let sectionContainer = document.querySelectorAll('section');
 
@@ -37,19 +18,6 @@ const removeActiveSection = (section) => {
     section.style.backgroundColor = "";
 };
 
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
- */
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
- */
 
 // build the nav
 
@@ -86,12 +54,6 @@ window.addEventListener('scroll', generateActiveSection);
 // Scroll to anchor ID using scrollTO event
 
 
-/**
- * End Main Functions
- * Begin Events
- * 
- */
-
 // Build menu 
 
 // Scroll to section on link click
@@ -105,6 +67,3 @@ const sectionScroll = () => {
     });
 }
 sectionScroll();
-
-
-// Set sections as active
